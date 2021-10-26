4c10ce18436e2573a851c13d84ae781fa77b131e
jimmylee <send@jimmy.email>
2021-10-25 17:19:08 -0700

    0.0.3 - stubs out car page

---
+++ pages/api-content-add-car.tsx
@@ -0,0 +1,44 @@
+import styles from '@pages/Page.module.scss';
+
+import * as React from 'react';
+
+import App from '@components/App';
+
+const markdown = `# ➟ /content/add-car
+
+WIP
+`;
+
+const key = `api-content-add-ipfs`;
+
+const code = `class Example extends React.Component {
+  componentDidMount() {
+    // TODO
+  }
+
+  render() {
+    return <pre>TODO</pre>;
+  }
+}`;
+
+function APIContentAddIPFS(props) {
+  return (
+    <App
+      title="Estuary Documentation: API: /content/add-car"
+      description="https://api.estuary.tech/content/add-car"
+      url={`https://docs.estuary.tech/${key}`}
+      active={key}
+      curl={curl}
+      markdown={markdown}
+      code={code}
+    />
+  );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {},
+  };
+}
+
+export default APIContentAddCAR;

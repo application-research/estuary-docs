d5458841138d8ac7cb00acf80781f8d94514c7e0
Sherif Louis <sherif@fil.org>
2021-10-27 12:48:08 -0700

    added add-car documentation

--- pages/api-content-add-car.tsx
+++ pages/api-content-add-car.tsx
@@ -5,22 +5,59 @@ import * as React from 'react';
 import App from '@components/App';
 
 const markdown = `# ➟ /content/add-car
+Use this endpoint to write a Content-Addressable Archive (CAR) file, and make storage deals for its contents.
+For more information on CARs check the [spec page](https://ipld.io/specs/transport/car/)
 
-WIP
+
+### This page is a work in progress
+
+We will be adding more code examples and more details over time. Thanks for bearing with us and our team! If you have ideas, write us some [feedback](https://docs.estuary.tech/feedback).
 `;
 
-const key = `api-content-add-ipfs`;
-const curl = null;
+
+
+
+const key = `api-content-add-car`;
+const curl = `curl -X POST https://api.estuary.tech/content/add-car -H "Authorization: Bearer REPLACE_ME_WITH_API_KEY" -H "Accept: application/json" --data-binary "@PATH_TO_FILE_BUT_REMEMBER_THE_@_SYMBOL_IS_REQUIRED"`;
 
 const code = `class Example extends React.Component {
-  componentDidMount() {
-    // TODO
+ upload(e) {
+    e.persist();
+    
+    // NOTE
+    // This example uses XMLHttpRequest() instead of fetch
+    // because we want to show progress. But you can use
+    // fetch in this example if you like.
+    const xhr = new XMLHttpRequest();
+    var url = "https://api.estuary.tech";
+    xhr.upload.onprogress = (e) => {
+      this.setState({ 
+        loaded: e.loaded, 
+        total: e.total 
+      });
+    }
+    
+    xhr.open(
+      "POST", 
+      url+"/content/add-car" 
+    );
+    xhr.setRequestHeader(
+      "Authorization", 
+      "Bearer REPLACE_ME_WITH_API_KEY"
+    );
+    xhr.send(e.target.files[0]);
   }
 
   render() {
-    return <pre>TODO</pre>;
+    return (
+      <React.Fragment>
+        <input type="file" onChange={this.upload.bind(this)} />
+        <br />
+        <pre>{JSON.stringify(this.state, null, 1)}</pre>
+      </React.Fragment>
+    );
   }
-}`;
+}`
 
 function APIContentAddCAR(props) {
   return (
@@ -43,3 +80,4 @@ export async function getServerSideProps(context) {
 }
 
 export default APIContentAddCAR;
+

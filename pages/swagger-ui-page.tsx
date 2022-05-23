809aa433f3d63c5926bd9e4f84e6fd2ace1648d7
alvin-reyes <me@areyes.onl>
2022-05-23 01:01:47 -0400

    chore: revert to ssr = false

--- pages/swagger-ui-page.tsx
+++ pages/swagger-ui-page.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css"
 
 function PageSwaggerTest(props) {
-  const SwaggerUI = dynamic(import('swagger-ui-react'), {ssr: true})
+  const SwaggerUI = dynamic(import('swagger-ui-react'), {ssr: false})
   return (
     // @ts-ignore
     <SwaggerUI url="https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json" docExpansion="list" deepLinking={true} />
